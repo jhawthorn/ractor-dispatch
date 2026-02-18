cc387d20bf187a6b6b6092ef90eb2f461565a12b
John Hawthorn <john@hawthorn.email>
2026-02-17 22:26:26 -0800

    Initial commit

---
+++ lib/ractor/dispatch.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "dispatch/version"
+
+module Ractor
+  module Dispatch
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
